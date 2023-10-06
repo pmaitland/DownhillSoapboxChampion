22b3471a09da092be0ee3628baede04c90410efe
pmaitland <pmaitland98@gmail.com>
2023-10-06 23:05:55 +0100

    stuff

--- DownhillSoapboxChampion/Scripts/Roads.gd
+++ DownhillSoapboxChampion/Scripts/Roads.gd
@@ -6,8 +6,13 @@ var roads : Array[Node3D] = []
 
 func _ready():
 	for i in range(1000):
-		var new_road = road_empty.instantiate()
-		add_child(new_road)
+		var new_left_road = road_empty.instantiate()
+		add_child(new_left_road)
 		if i > 0:
-			new_road.position = roads[i-1].get_node("AttachPoint").global_position
-		roads.append(new_road)
+			new_left_road.position = roads[i-1].get_node("AttachPoint").global_position
+		roads.append(new_left_road)
+		for j in range(4):
+			var new_road = road_empty.instantiate()
+			add_child(new_road)
+			new_road.position = new_left_road.position
+			new_road.position.x += j+1
--- DownhillSoapboxChampion/Scripts/Soapbox.gd
+++ DownhillSoapboxChampion/Scripts/Soapbox.gd
@@ -1,12 +1,26 @@
 extends RigidBody3D
 
+var speed = 1.5
+
+var default_rotation = 180 * PI/180
+var turn_amount = 40 * PI/180
+
+var default_position = 0
+var shift_amount = 0.1
 
 func _physics_process(delta):
+	rotation.x = 0
+	rotation.y = 0
+	rotation.z = 0
+	
 	if Input.is_action_pressed("Left"):
-		position.x -= 0.6 * delta
-		get_node("Soapbox-0").rotation.y = 140 * PI/180
+		position.x -= speed * delta
+		get_node("Soapbox-0").rotation.y = default_rotation - turn_amount
+		get_node("Soapbox-0").position.x = default_position - shift_amount
 	elif Input.is_action_pressed("Right"):
-		position.x += 0.6 * delta
-		get_node("Soapbox-0").rotation.y = 220 * PI/180
-	else:
-		get_node("Soapbox-0").rotation.y = 180 * PI/180
+		position.x += speed * delta
+		get_node("Soapbox-0").rotation.y = default_rotation + turn_amount
+		get_node("Soapbox-0").position.x = default_position + shift_amount
+	elif Input.is_action_just_released("Left") or Input.is_action_just_released("Right"):
+		get_node("Soapbox-0").rotation.y = default_rotation
+		get_node("Soapbox-0").position.x = default_position
