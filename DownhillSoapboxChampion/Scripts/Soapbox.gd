1fb0c7b71808d1253fc30aeb2a66b2db0773fda2
pmaitland <pmaitland98@gmail.com>
2023-10-06 21:45:01 +0100

    soapbox things

--- DownhillSoapboxChampion/Scripts/Soapbox.gd
+++ DownhillSoapboxChampion/Scripts/Soapbox.gd
@@ -1,9 +1,12 @@
-extends CharacterBody3D
-
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+extends RigidBody3D
 
 
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-	move_and_slide()
+	if Input.is_action_pressed("Left"):
+		position.x -= 0.6 * delta
+		get_node("Soapbox-0").rotation.y = 140 * PI/180
+	elif Input.is_action_pressed("Right"):
+		position.x += 0.6 * delta
+		get_node("Soapbox-0").rotation.y = 220 * PI/180
+	else:
+		get_node("Soapbox-0").rotation.y = 180 * PI/180
