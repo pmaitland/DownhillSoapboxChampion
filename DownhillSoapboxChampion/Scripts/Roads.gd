0291b1f4f65c0cd65cc09602f99e2066c743c6be
pmaitland <pmaitland98@gmail.com>
2023-10-07 18:13:12 +0100

    added haystacks to level

--- DownhillSoapboxChampion/Scripts/Roads.gd
+++ DownhillSoapboxChampion/Scripts/Roads.gd
@@ -6,15 +6,30 @@ enum RoadType {
 	EMPTY
 }
 
+var haystack = load("res://Prefabs/Haystack.tscn")
+
 var gridmap : GridMap
+
+var rng = RandomNumberGenerator.new()
 
 func _ready():
 	gridmap = get_node("GridMap")
-
-func _process(delta):
 	for z in range(1000):
 		for x in range(5):
 			if x == 2:
 				gridmap.set_cell_item(Vector3i(x, 0, z), RoadType.EMPTY_CENTRE)
 			else:
 				gridmap.set_cell_item(Vector3i(x, 0, z), RoadType.EMPTY)
+				
+			if x == 1 or x == 2 or x == 3:
+				if rng.randi_range(1, 10) == 1:
+					place_obstacle(haystack, x, z)
+
+func place_obstacle(scene, x, z):
+	var new_scene = scene.instantiate()
+	var new_position = gridmap.map_to_local(Vector3(x, 0, z))
+	new_position.x += rng.randf_range(-1, 1)
+	new_position.z += rng.randf_range(-1, 1)
+	new_scene.position = new_position
+	new_scene.rotation.y = rng.randf_range(0, 2*PI)
+	add_child(new_scene)
