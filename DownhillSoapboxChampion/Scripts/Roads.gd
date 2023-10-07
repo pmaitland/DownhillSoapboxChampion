e684f1ace93f78289ee58eda9552befe3a3ade40
Yasmojam <yazzij97@gmail.com>
2023-10-08 00:31:04 +0100

    ramp and cone

--- DownhillSoapboxChampion/Scripts/Roads.gd
+++ DownhillSoapboxChampion/Scripts/Roads.gd
@@ -9,6 +9,7 @@ enum RoadType {
 }
 
 @export var obstacles: Array[PackedScene]
+var ramp_scene = load("res://Prefabs/Ramp.tscn")
 
 var gridmap : GridMap
 
@@ -29,10 +30,15 @@ func _ready():
 					gridmap.set_cell_item(Vector3i(x, 0, z), RoadType.EMPTY)
 				
 			if x == 1 or x == 2 or x == 3:
-				# place random obstacle 
-				if rng.randi_range(1, 10) == 1:
+				var obstacle_chance = rng.randi_range(1, 10)
+				var ramp_chance = rng.randi_range(1, 30)
+				# place random obstacle 10% of the time 
+				if obstacle_chance == 1:
 					place_obstacle(obstacles[randi_range(0, obstacles.size()-1)], x, z) 
-
+				else:
+					if ramp_chance == 1:
+						place_ramp(x, z) 
+					
 func place_obstacle(scene, x, z):
 	var new_scene = scene.instantiate() 
 	var new_position = gridmap.map_to_local(Vector3(x, 0, z))
@@ -41,3 +47,11 @@ func place_obstacle(scene, x, z):
 	new_scene.position = new_position
 	new_scene.rotation.y = rng.randf_range(0, 2*PI)
 	add_child(new_scene)
+	
+func place_ramp(x, z):
+	var new_ramp = ramp_scene.instantiate()
+	var new_position = gridmap.map_to_local(Vector3(x, 0, z))
+	new_position.x += rng.randf_range(-1, 1)
+	new_position.z += rng.randf_range(-1, 1)
+	new_ramp.position = new_position
+	add_child(new_ramp)
